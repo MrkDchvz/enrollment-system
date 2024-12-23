cf4f6a371f302e9c84f6c2179a2c46ec74acae3b
Mark D. <82815895+MrkDchvz@users.noreply.github.com>
2024-12-23 21:35:50 +0800

    finished seeders for courses and populators

--- database/seeders/PopulatorSeeder.php
+++ database/seeders/PopulatorSeeder.php
@@ -83,27 +83,88 @@ class PopulatorSeeder extends Seeder
             ['course_id' => $courseMapping['ITEC 55'], 'semester' => '1st Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
 
             // BSCS 2ND YEAR, 2ND SEM SUBJECTS
-            ['course_code' => $courseMapping['GNED 08'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
-            ['course_code' => $courseMapping['DCIT 25'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
-            ['course_code' => $courseMapping['DCIT 55'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
-            ['course_code' => $courseMapping['FITT 4'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['GNED 08'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['DCIT 25'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['DCIT 55'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['FITT 4'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
             ['course_id' => $courseMapping['GNED 14'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
-            ['course_code' => $courseMapping['MATH 02'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
-            ['course_code' => $courseMapping['COSC 65'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
-            ['course_code' => $courseMapping['COSC 70'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['MATH 02'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 65'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 70'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSCS'],
 
             // BSIT 2ND YEAR, 2ND SEM SUBJECTS
-            ['course_code' => $courseMapping['GNED 08'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
-            ['course_code' => $courseMapping['DCIT 25'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
-            ['course_code' => $courseMapping['DCIT 55'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
-            ['course_code' => $courseMapping['FITT 4'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
-            ['course_code' => $courseMapping['ITEC 60'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
-            ['course_code' => $courseMapping['ITEC 65'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
-            ['course_code' => $courseMapping['ITEC 70'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['GNED 08'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['DCIT 25'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['DCIT 55'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['FITT 4'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 60'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 65'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 70'], 'semester' => '2nd Semester', 'year_level' => '2nd Year', 'program' => 'BSIT'],
+
+            // BSCS 3RD YEAR, 1ST SEM SUBJECTS
+            ['course_id' => $courseMapping['DCIT 65'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['DCIT 26'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['MATH 3'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 75'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 80'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COCS 101'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 85'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+
+            // BSIT 3RD YEAR, 1ST SEM SUBJECTS
+            ['course_id' => $courseMapping['DCIT 26'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 85'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 80'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 90'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['INSY 55'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['DCIT 60'], 'semester' => '1st Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+
+            // BSCS 3RD YEAR, 2ND SEM SUBJECTS
+            ['course_id' => $courseMapping['ITEC 85'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['DCIT 60'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['GNED 09'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['MATH 4'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 90'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 95'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 106'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSCS'],
+
+            // BSIT 3RD YEAR, 2ND SEM SUBJECTS
+            ['course_id' => $courseMapping['GNED 09'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 95'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 101'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 106'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 100'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 200A'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
+
+            // BSCS 4TH YEAR, 1ST SEM SUBJECTS
+            ['course_id' => $courseMapping['ITEC 80'], 'semester' => '1st Semester', 'year_level' => '4th Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 100'], 'semester' => '1st Semester', 'year_level' => '4th Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 105'], 'semester' => '1st Semester', 'year_level' => '4th Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 111'], 'semester' => '1st Semester', 'year_level' => '4th Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 200A'], 'semester' => '1st Semester', 'year_level' => '4th Year', 'program' => 'BSCS'],
+
+            // BSIT 4TH YEAR, 1ST SEM SUBJECTS
+            ['course_id' => $courseMapping['DCIT 65'], 'semester' => '1st Semester', 'year_level' => '4th Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 111'], 'semester' => '1st Semester', 'year_level' => '4th Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 116'], 'semester' => '1st Semester', 'year_level' => '4th Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 110'], 'semester' => '1st Semester', 'year_level' => '4th Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 200B'], 'semester' => '1st Semester', 'year_level' => '4th Year', 'program' => 'BSIT'],
+
+            // BSCS 4TH YEAR, 2ND SEM SUBJECTS
+            ['course_id' => $courseMapping['GNED 07'], 'semester' => '2nd Semester', 'year_level' => '4th Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['GNED 10'], 'semester' => '2nd Semester', 'year_level' => '4th Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 110'], 'semester' => '2nd Semester', 'year_level' => '4th Year', 'program' => 'BSCS'],
+            ['course_id' => $courseMapping['COSC 200B'], 'semester' => '2nd Semester', 'year_level' => '4th Year', 'program' => 'BSCS'],
+
+            // BSIT 4TH YEAR, 2ND SEM SUBJECTS
+            ['course_id' => $courseMapping['ITEC 199'], 'semester' => '2nd Semester', 'year_level' => '3rd Year', 'program' => 'BSIT'],
 
             // BSCS MIDYEAR
+            ['course_id' => $courseMapping['COSC 199'], 'semester' => null, 'year_level' => 'Mid Year', 'program' => 'BSCS'],
 
             // BSIT MIDYEAR
+            ['course_id' => $courseMapping['STAT 2'], 'semester' => null, 'year_level' => 'Mid Year', 'program' => 'BSIT'],
+            ['course_id' => $courseMapping['ITEC 75'], 'semester' => null, 'year_level' => 'Mid Year', 'program' => 'BSIT'],
+
 
 
 
